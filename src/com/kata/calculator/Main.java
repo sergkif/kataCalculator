7e6a8121e884086dce55a24066d4b5fbbc2e437d
sergkif <sergkif2013@gmail.com>
2018-09-27 19:20:04 +0300

    Better code

--- src/com/kata/calculator/Main.java
+++ src/com/kata/calculator/Main.java
@@ -1,45 +1,14 @@
 package com.kata.calculator;
-import com.kata.calculator.Node;
+
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static com.kata.calculator.Node createNewNode(Integer token) {
-        com.kata.calculator.Node newNode = new com.kata.calculator.Node();
-        newNode.token = token;
-        newNode.left = newNode.right = null;
-        return newNode;
-    }
-
-    public static com.kata.calculator.Node Insert(com.kata.calculator.Node root, Integer token) {
-        if (root == null) {
-            root = createNewNode(token);
-        } else if (token <= root.token) {
-            root.left = Insert(root.left, token);
-        } else {
-            root.right = Insert(root.right, token);
-        }
-        return root;
-    }
-
-    public static void print(com.kata.calculator.Node root) {
-        if (root == null) return;
-        com.kata.calculator.Node leftNode = root.left;
-        print(leftNode);
-        System.out.print(root.token + " ");
-        com.kata.calculator.Node rightNode = root.right;
-        print(rightNode);
-    }
-
     public static void main(String[] args) {
-        Node root = null;
-        root = Insert(root, 15);
-        root = Insert(root, 10);
-        root = Insert(root, 20);
-        root = Insert(root, 30);
-        root = Insert(root, 5);
-        root = Insert(root, 100);
-        root = Insert(root, 0);
-        root = Insert(root, -10);
-        print(root);
+        String str = "3 5 10 200 394 23 -20 0 1";
+        Node root = new Node();
+        Stream.of(str.split(" ")).forEach(root::insert);
+
+        System.out.println(root.toString());
     }
 }
--- src/com/kata/calculator/Node.java
+++ src/com/kata/calculator/Node.java
@@ -1,7 +1,31 @@
 package com.kata.calculator;
 
 public class Node {
-    public Integer token;
-    public com.kata.calculator.Node left;
-    public com.kata.calculator.Node right;
+    public String token;
+    public Node left;
+    public Node right;
+
+    public Node() {
+    }
+
+    @Override
+    public String toString() {
+        String left = this.left==null?"":this.left.toString();
+        String right = this.right==null?"":this.right.toString();
+        return String.valueOf(left) + " "  + this.token + " "  + right;
+    }
+
+    public void insert(String token) {
+        if (this.token == null) this.token = token;
+        if (Integer.valueOf(token) <= Integer.valueOf(this.token)) this.left = checkNode(this.left, token);
+        else this.right = checkNode(this.right, token);
+    }
+
+    public Node checkNode(Node node, String token) {
+        if (node == null) {
+            node = new Node();
+            node.token = token;
+        } else node.insert(token);
+        return node;
+    }
 }
